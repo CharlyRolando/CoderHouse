3d2bf5186915efe3eb3ec884877902c9b1742788
crolando <crolando@SELMA-APG>
2022-09-25 01:43:57 -0300

    Nuevo desafío

--- Desafios/1-Componentes_Rolando/src/app/app.module.ts
+++ Desafios/1-Componentes_Rolando/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { SidebarComponent } from './componets/sidebar/sidebar.component';
+import { MainComponent } from './componets/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
